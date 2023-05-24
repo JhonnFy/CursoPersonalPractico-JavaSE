586b69dac406a4b415b319b078de43a987cb3142
JhonFy <dijhonsdeveloper1023@gmail.com>
2023-05-24 15:30:37 -0500

    Operador2

---
+++ CursoPersonalPracticoJavaSE/src/Operadores/Operador2.java
@@ -0,0 +1,16 @@
+package Operadores;
+
+import java.util.*;
+import java.math.*;
+
+public class Operador2 {
+
+    public static void main(String[] args) {
+
+        int numero = 10;
+        numero = (numero + 5);
+
+        System.out.print("\n\tValor Del Numero " + numero + ", Aumentado");
+        System.out.print("\n\t");
+    }
+}
