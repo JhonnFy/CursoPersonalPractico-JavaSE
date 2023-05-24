ccacbf2e4cae3663350d06b3a807ea692137e62e
JhonFy <dijhonsdeveloper1023@gmail.com>
2023-05-24 10:50:05 -0500

    HolaMundo

---
+++ CursoPersonalPracticoJavaSE/src/CursoPersonalPracticoJavaSE/Algoritmo1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CursoPersonalPracticoJavaSE;
+import java.util.*;
+import java.math.*;
+
+public class Algoritmo1 {
+    public static void main(String[] args) {
+     
+        Scanner Alg1 = new Scanner(System.in);
+        
+        System.out.print("\n\tHola Mundo");
+        
+        
+    }
+}
