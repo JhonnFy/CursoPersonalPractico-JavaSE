c00e32495a418f11c8ae283690b9c3bf544b6927
JhonFy <dijhonsdeveloper1023@gmail.com>
2023-05-24 12:12:21 -0500

    Algoritmo5

---
+++ CursoPersonalPracticoJavaSE/src/CursoPersonalPracticoJavaSE/Algoritmo5.java
@@ -0,0 +1,21 @@
+package CursoPersonalPracticoJavaSE;
+
+import java.util.*;
+import java.math.*;
+
+public class Algoritmo5 {
+
+    public static void main(String[] args) {
+
+        Scanner Alg5 = new Scanner(System.in);
+
+        //Constante
+        final float nUno = 20.5f;
+
+               
+        System.out.print("\n\n\t*****Tipos De Datos Constante*****");
+        System.out.print("\n\t1-Valor Constante\t" + nUno + ".");
+        System.out.print("\n\t");
+        
+    }
+}
