a44e4447d09a47edf727ca71c4db6212271450ed
JhonFy <dijhonsdeveloper1023@gmail.com>
2023-05-24 16:12:43 -0500

    Operador3

---
+++ CursoPersonalPracticoJavaSE/src/Operadores/Operador3.java
@@ -0,0 +1,17 @@
+package Operadores;
+import java.util.*;
+import java.math.*;
+
+public class Operador3 {
+    public static void main(String[] args) {
+     
+        int x = 5;
+                
+        System.out.print("\n\t*****OPERADOR DE INCREMENTO*****");
+        x++;
+        System.out.print("\n\tIncremento\t\t"+x+".");
+        x--;
+        System.out.print("\n\tDecremento\t\t"+x+".");
+        System.out.print("\n\t");
+    }
+}
