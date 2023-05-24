9d2ad61c665505f0dc312e98586ca4404a9df363
JhonFy <dijhonsdeveloper1023@gmail.com>
2023-05-24 11:10:08 -0500

    Algoritmo2

---
+++ CursoPersonalPracticoJavaSE/src/CursoPersonalPracticoJavaSE/Algoritmo2.java
@@ -0,0 +1,23 @@
+package CursoPersonalPracticoJavaSE;
+
+//Librerias
+import java.util.*;
+import java.math.*;
+
+/*Algoritmo que solicite su nombre*/
+
+
+public class Algoritmo2 {
+    public static void main(String[] args) {
+        
+        //Objeto Scanner
+        Scanner Alg2 = new Scanner(System.in);
+        
+        //Variable nombre
+        String nombre = "" ;
+        
+        System.out.print("\n\t¿Su nombre, por favor?\t\t");
+        Alg2.nextLine();
+        System.out.print("\n\t");
+    }
+}
