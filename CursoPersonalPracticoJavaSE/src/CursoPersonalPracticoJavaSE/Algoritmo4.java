b9f609325e035ba84f4a42b032d30be5844c8645
JhonFy <dijhonsdeveloper1023@gmail.com>
2023-05-24 12:06:15 -0500

    Algoritmo4

---
+++ CursoPersonalPracticoJavaSE/src/CursoPersonalPracticoJavaSE/Algoritmo4.java
@@ -0,0 +1,17 @@
+package CursoPersonalPracticoJavaSE;
+import java.util.*;
+import java.math.*;
+
+public class Algoritmo4 {
+    public static void main(String[] args) {
+     
+        Scanner Alg4 = new Scanner(System.in);
+        
+        Integer nUno = null;
+        
+        System.out.print("\n\n\t*****Tipos De Datos No Primitivo*****");
+        System.out.print("\n\t1-Valor Primitivo\t" + nUno + ".");
+        System.out.print("\n\t");
+        
+    }
+}
