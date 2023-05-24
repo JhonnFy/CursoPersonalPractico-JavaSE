b4970468c004a9af28c13b9e277a4aea11ff307e
JhonFy <dijhonsdeveloper1023@gmail.com>
2023-05-24 13:54:26 -0500

    Algoritmo7

---
+++ CursoPersonalPracticoJavaSE/src/CursoPersonalPracticoJavaSE/Algoritmo7.java
@@ -0,0 +1,34 @@
+package CursoPersonalPracticoJavaSE;
+
+import java.util.*;
+import java.math.*;
+import javax.swing.*;
+
+public class Algoritmo7 {
+
+    public static void main(String[] args) {
+
+        int ventera;
+        double vdecimal;
+        String cadena;
+        char caracter;
+
+        ventera = Integer.parseInt(JOptionPane.showInputDialog("Ingrese Un Entero"));
+        vdecimal = Double.parseDouble(JOptionPane.showInputDialog("Ingrese Un Decimal"));
+        cadena = JOptionPane.showInputDialog("Ingrese Una Cadena");
+        caracter = JOptionPane.showInputDialog("Ingrese Un Caracter").charAt(0);
+        
+        JOptionPane.showMessageDialog(null, "Valor Entero"+ventera);
+        JOptionPane.showMessageDialog(null,"Valor Decimal"+ ventera);
+        JOptionPane.showMessageDialog(null,"Valor Cadena"+cadena);
+        JOptionPane.showMessageDialog(null, "Valor Caracter"+caracter);
+        
+        System.out.print("\n\t*****DATOS INGRESADOS*****");
+        System.out.print("\n\tValor Entero\t\t"+ventera+".");
+        System.out.print("\n\tValor Decimal\t\t"+vdecimal+".");
+        System.out.print("\n\tValor Cadena\t\t"+cadena+".");
+        System.out.print("\n\tValor Caracter\t\t"+caracter+".");
+        System.out.print("\n\t");
+        
+    }
+}
