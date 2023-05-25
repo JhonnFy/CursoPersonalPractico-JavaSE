efb96a3974e2897f740182426d30241568840962
JhonFy <dijhonsdeveloper1023@gmail.com>
2023-05-25 17:24:03 -0500

    Algoritmo7

--- CursoPersonalPracticoJavaSE/src/Algoritmos/Algoritmo7.java
+++ CursoPersonalPracticoJavaSE/src/Algoritmos/Algoritmo7.java
@@ -12,9 +12,26 @@ import java.math.*;
 public class Algoritmo7 {
     public static void main(String[] args) {
         
+        Scanner Alg7 = new Scanner(System.in);
+        
         int nth = 0;
         int sms = 0;
         int dias = 0;
+        int hrs = 0;
         
+        System.out.print("\n\t*****HORAS PARA SEMANAS*****");
+        System.out.print("\n\t¿Cantidad De Horas?\t\t");
+        nth = Alg7.nextInt();
+     
+        //24 Horas Día * 7 1 Semana
+        sms = (nth / 168);
+        dias = (nth%168)/24;
+        hrs = nth%24;
+        
+        System.out.print("\n\t*****SALIDA DE DATOS*****");
+        System.out.print("\n\tSemanas\t\t\t"+sms+".");
+        System.out.print("\n\tDías\t\t\t"+dias+".");
+        System.out.print("\n\tHoras Equivalentes\t"+hrs+".");
+        System.out.print("\n\t");
     }
 }
--- CursoPersonalPracticoJavaSE/src/Algoritmos/Algoritmo8.java
+++
@@ -1,8 +0,0 @@
-
-package Algoritmos;
-
-public class Algoritmo8 {
-    public static void main(String[] args) {
-        
-    }
-}
