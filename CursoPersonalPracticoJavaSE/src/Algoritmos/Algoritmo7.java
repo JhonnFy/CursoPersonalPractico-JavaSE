d7c5b36978db65b2fa89e4f928f109b303b640ee
JhonFy <dijhonsdeveloper1023@gmail.com>
2023-05-25 16:58:50 -0500

    Algoritmo7

---
+++ CursoPersonalPracticoJavaSE/src/Algoritmos/Algoritmo7.java
@@ -0,0 +1,20 @@
+/*Construir un programa que, dado un número total de horas, 
+devuelve el número de:
+Semanas, 
+Días,
+Horas Equivalentes.
+Por ejemplo, dado un total de 1000 horas, debe mostrar 5 semanas, 6 días, 16 horas.*/
+
+package Algoritmos;
+import java.util.*;
+import java.math.*;
+
+public class Algoritmo7 {
+    public static void main(String[] args) {
+        
+        int nth = 0;
+        int sms = 0;
+        int dias = 0;
+        
+    }
+}
